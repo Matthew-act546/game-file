cf7ebaae286f5f6b93f3e6bfb3a24560fa8f998f
matthew <matthewyt765@gmail.com>
2023-11-23 21:36:21 +0800

    updated the guessing game fix some bugs and update readme

--- README.md
+++ README.md
@@ -2,6 +2,13 @@
 
 This repository is a gaming project of my own.
 
+
+## Guessing Game
+What number matthew thinking?
+There is noob matthew, pro matthew and lastly a hacker matthew.
+[![guessingGamePic](guessIntro.png)](https://github.com/Matthew-act546/game-file)
+
+
 ### NOTE!!!
 This *Gaming Programming* program/code is my self project to exercise my programming skills.
 
--- guessing_game.py
+++ guessing_game.py
@@ -1,74 +1,132 @@
 from pyfiglet import figlet_format
 from random import randint
 
+description = """
+Game Mechanics!
+
+Guess the number what matthew thinking in his mind.
+NOOB(EASY) LEVEL OR BABY MATTHEW LEVEL HAVE A LIMITATION OF 10 IN HIS MIND IF YOU SOLVED THIS YOU ARE NOOB!
+PRO(NORMAL) LEVEL OR TEEN MATTHEW LEVEL HAVE A LIMTAITION OF 30 NUMBERS IN HIS MIND IF YOU SOLVED THIS YOU ARE PRO!
+HACKER(HARD) LEVEL OR ADULT MATTHEW LEVEL HAVE A LIMITATION OF 50 NUMVERS IN HIS MIND IF YOU SOLVED THIS YOU ARE HACKER!"""
+
+print(''.center(130, '_'))
+print(figlet_format("Guess Number") + description)
+print(''.center(130, '_')) 
+
 def main():
     """Main game file of Guessing Game."""
-    print(''.center(100, '_'))
-    print(figlet_format("Guessing Game"))
-    print(''.center(100, '_')) 
-    level_text = """    
-Choose a level:
-    [1] Easy
-    [2] Normal
-    [3] Hard
+    
+    level_text = """Levels:
+[1] NOOB
+[2] PRO
+[3] HACKER
 """
     try:
-        level_chosed = int(input(level_text + "---> "))
+        level_chosed = int(input(level_text + "Choose Level: "))
     except ValueError:
         print("Please enter an integer ")
         main()
     else:
         if level_chosed == 1:
+            attempt = 0
+            lives = 3
             answer = randint(1, 10)
-            guess_num = int(input("What is your guess number(1-10): "))
-            while guess_num != answer:
-                if guess_num < answer:
-                    print("Higher")
+            while attempt < lives:
+                try:
                     guess_num = int(input("What is your guess number(1-10): "))
-                elif guess_num > answer:
-                    print("lower")
-                    guess_num = int(input("What is your guess number(1-10): "))
-                else:
-                    print("level exceeded\nYou die!")
-            if guess_num == answer:
-                    print("Your Guess is right!!!")
-                    play_again = input("Do you want to play again?(Y/N): ")
-                    if play_again.lower() == 'y':
-                        main()
-                    elif play_again.lower() == 'n':
-                        print(figlet_format("Thank you for playing", font="slant"))
+                    if 1 <= guess_num <= 10:
+                        if guess_num == answer:
+                            print("Your Guess is right!!!")
+                            play_again = input("Do you want to play again?(Y/N): ")
+                            if play_again.lower() == 'y':
+                                play()
+                            elif play_again.lower() == 'n':
+                                print(figlet_format("Thank you for playing", font="slant"))
+                                attempt = 4
+                            else:
+                                print("get loss")
+                        elif guess_num < answer:
+                            print("Higher")
+                            attempt += 1
+                        elif guess_num > answer:
+                            print("Lower")
+                            attempt += 1
                     else:
-                        print("get loss")
-
+                        print("Please enter 1 to 10")
+                except ValueError:
+                    print("Enter a number")
+            if attempt == 3:
+                print(figlet_format("YOU DIE!!!"))
+                
         elif level_chosed == 2:
+            attempt = 0
+            lives = 3
             answer = randint(1, 30)
-            guess_num = int(input("What is your guess number(1-30): "))
-            while guess_num != answer:
-                if guess_num < answer:
-                    print("Higher")
+            while attempt < lives:
+                try:
                     guess_num = int(input("What is your guess number(1-30): "))
-                elif guess_num > answer:
-                    print("lower")
-                    guess_num = int(input("What is your guess number(1-30): "))
-                else:
-                    print("level exceeded\nYou die!")
-            if guess_num == answer:
-                    print("Your Guess is right!!!")
-                    play_again = input("Do you want to play again?(Y/N): ")
-                    if play_again.lower() == 'y':
-                        main()
-                    elif play_again.lower() == 'n':
-                        print(figlet_format("Thank you for playing", font="slant"))
+                    if 1 <= guess_num <= 30:
+                        if guess_num == answer:
+                            print("Your Guess is right!!!")
+                            play_again = input("Do you want to play again?(Y/N): ")
+                            if play_again.lower() == 'y':
+                                play()
+                            elif play_again.lower() == 'n':
+                                print(figlet_format("Thank you for playing", font="slant"))
+                                attempt = 4
+                            else:
+                                print("get loss")
+                        elif guess_num < answer:
+                            print("Higher")
+                            attempt += 1
+                        elif guess_num > answer:
+                            print("Lower")
+                            attempt += 1
                     else:
-                        print("get loss")
-        elif level_chosed == 3:
-            answer = randint(1, 50)
-            print("This is a one time only go be a magician guess that number!")
-            guess_num = int(input("What is your guess number(1-50): "))
-            if guess_num == answer:
-                print("Your Guess is right!!!")
-        else:
-            print("maintenance")
+                        print("Please enter 1 to 30")
+                except ValueError:
+                    print("Enter a number")
+            if attempt == 3:
+                print(figlet_format("YOU DIE!!!"))
 
-    
+        elif level_chosed == 3:
+            attempt = 0
+            lives = 3
+            answer = randint(1, 10)
+            while attempt < lives:
+                try:
+                    guess_num = int(input("What is your guess number(1-10): "))
+                    if 1 <= guess_num <= 30:
+                        if guess_num == answer:
+                            print("Your Guess is right!!!")
+                            play_again = input("Do you want to play again?(Y/N): ")
+                            if play_again.lower() == 'y':
+                                play()
+                            elif play_again.lower() == 'n':
+                                print(figlet_format("Thank you for playing", font="slant"))
+                                attempt = 4
+                            else:
+                                print("get loss")
+                        elif guess_num < answer:
+                            print("Higher")
+                            attempt += 1
+                        elif guess_num > answer:
+                            print("Lower")
+                            attempt += 1
+                    else:
+                        print("Please enter 1 to 10")
+                except ValueError:
+                    print("Enter a number")
+            if attempt == 3:
+                print(figlet_format("YOU DIE!!!"))
+        else:
+            print("Enter a number corresponding to the levels")
+            main()
+
+
+def play():
+    """playing again the game with welcome back!"""
+    print(figlet_format("Welcome back!"))
+    main()
+
 main()
